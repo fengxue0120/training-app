af8732ea4eebec7e22f1d6f7fc80ffb826aadd87
fengxue-bit <2192057409@qq.com>
2019-12-19 09:58:19 +0800

    init training project

---
+++ src/main/java/controller/HelloController.java
@@ -0,0 +1,20 @@
+package controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * 基于SSM框架的第一个控制器
+ * @author GaoJieRu
+ * @since  jdk8
+ */
+@Controller
+public class HelloController {
+	
+    @RequestMapping("/hi")
+    @ResponseBody
+	public String hello() {
+		  return "hello nuc-a";
+	}
+}
